c4d42561953803b1e56f5e4898c61d4c1174b3f1
Pedro Rios v <pv123124@gmail.com>
2021-02-20 02:11:10 -0600

    ○ Se agrego SSTF.py que contiene las funciones: 	• SSTF: Ejecuta el metodo SSTF 	• shortest_pointer: Encuentra la siguiente peticion en cola.
    ○ Se agrego main.py la cual contiene la clase Main que contiene las funciones:
    	• print_tableau: Para imprimir los resultados de los metodos.
    	• ingreso_num_peticiones: Para ingresar la cantidad de peticiones a ingresar.
    	• validacion_peticion: Para validar que la peticion se encuentre en el rango adecuado.
    	• ingreso_peticion_inicial.
    	• ingreso_peticiones.
    	• metodos: Para ejecutar el metodo adecuado segun la opcion seleccionada en el menu
    	• menu.
    	• init: Metodo principal para la ejecucion del programa.

---
+++ SSTF.py
@@ -0,0 +1,73 @@
+import areli
+
+
+def SSTF(input_array, inicial):
+    print("\n==============================   SHORTEST SEEK TIME FIRST   ==============================\n")
+    entry = areli.quickSort(input_array)    # entry es el arreglo de datos ordenados de menor a mayor
+    pointer = inicial
+
+    actual = []         # Columna : Cilindro actual
+    solicitado = []     # Columna : Cilindro solicitado / siguiente
+    tiempo_espera = []  # Columna : Cilindro tiempo de espera
+    desplazamiento = [] # Columna : Desplazamiento
+
+    for i in range(len(entry)):
+        actual.append(pointer)
+
+        if(i == 0):
+            tiempo_espera.append(0)
+        else:
+            tiempo_espera.append(tiempo_espera[i-1] + desplazamiento[i-1])
+
+        # Retorna los valores a la izquierda y derecha del valor puntero
+        izq, der = areli.puntoMedio(entry, pointer)
+        # Retorna el cilindro siguiente y el desplazamiento que va a realizar
+        next_cil, mov = shortest_pointer(pointer, izq, der)
+
+        solicitado.append(next_cil)
+        desplazamiento.append(mov)
+
+        # Se elimina del arreglo el cilindro que ya ha sido procesado
+        entry.pop(entry.index(next_cil))
+
+        pointer = next_cil
+        pass
+    return [actual, solicitado, tiempo_espera, desplazamiento]
+    pass
+
+def shortest_pointer(pointer, izq, der):
+    # Se valida si el izquierdo es nulo, si lo es, se verifica que derecho no sea nulo,
+    # si derecho es nulo se retornan valores nulos
+    if(izq == None):
+        if(der != None):
+            return der, (der - pointer)
+        else:
+            return None, None
+    # se valida si el derecho es nulo, si lo es, se verifica que izquierdo no sea nulo,
+    # si izquierdo es nulo se retornan valores nulos
+    if(der == None):
+        if(izq != None):
+            return izq, (pointer - izq)
+        else:
+            return None, None
+
+    # Si ambos no son nulos se procede a restar
+    dif_point_izq = pointer - izq   # El izquierdo es menor que el puntero, por ello,
+                                    # para obtener un valor positivo se puso a la derecha el izquierdo
+    dif_point_der = der - pointer   # El puntero es menor al derecho, por ello,
+                                    # para obtener un valor positivo se puso a la izquierda el puntero
+
+    if(dif_point_izq < dif_point_der):
+        return izq, dif_point_izq
+
+    return der, dif_point_der
+    pass
+
+"""
+arreglo = [98,183,37,122,14,124,65,67]
+tableau = SSTF(arreglo,53)
+print("Cilindro actual : \t\t", tableau[0])
+print("Cilindro solicitado : \t\t", tableau[1])
+print("Tiempo de espera : \t\t", tableau[2])
+print("Desplazamiento : \t\t", tableau[3])
+"""
---
+++ main.py
@@ -0,0 +1,169 @@
+# DATOS DE ENTRADA
+#   • El numero de cilindros va a ser del 0 al 199
+#   • El usuario debera brindar el numero de peticiones (buffer de peticiones) Ej. 5. ←
+#       - Puede ser que el usuario no brinde el numero de peticiones, entonces el limite sera   [DESCARTADO]
+#         cuando el usuario ingrese un numero negativo o un "-1"                                [DESCARTADO]
+#   • El usuario debera brindar los valores de las "n" peticiones que especifico
+#       - Validar que los valores no sean menores a 0 ni mayores a 199
+#   • El usuario proporcionara el cilindro inicial (validar)
+#
+# ================================================================================================
+#              DATOS DEL LA INTERFAZ DE COMANDOS
+#
+#   ♦ Hacer un menu de seleccion para los diferentes algoritmos
+#       • FCFS      [1]
+#       • SSTF      [2]
+#       • SCAN      [3]
+#       • C-SCAN    [4]
+#       • LOOK      [5]
+#       • C-LOOK    [6]
+#       • Salir     [7]
+#
+#   ♦ Mostrar la tabla de iteraciones mostrando los pasos realizados
+#   ♦ Mostrar los resultados finales
+#       • Desplazamiento
+#       • Tiempo Promedio de Espera
+#
+# ================================================================================================
+
+import SSTF
+
+
+class Main:
+    inicial = 53
+    isDone = False
+    buffer_datos = [98, 183, 37, 122, 14, 124, 65, 67]
+    tableau = []
+
+    def print_tableau(self, tableau):
+        actual = tableau[0]
+        solicitado = tableau[1]
+        tiempo_espera = tableau[2]
+        desplazamiento = tableau[3]
+
+        length = len(actual)
+
+        print("Cilindro Actual\t\tCilindro solicitado\t Tiempo de espera\tDesplazamiento")
+        for i in range(length):
+            print('     ', actual[i], '\t\t       ', solicitado[i], '\t\t       ', tiempo_espera[i],
+                  '\t\t     ', desplazamiento[i])
+        print("\n==========================================================================================\n")
+        pass
+
+    def ingreso_num_peticiones(self):
+        num_peticiones = None
+        while (True):
+            try:
+                num_peticiones = int(input('Cuantas peticiones va a ingresar? : '))
+                break
+            except ValueError:
+                print('\nEl valor que ingreso no es un numero, por favor, vuelva a intentarlo.\n')
+                continue
+            pass
+        return num_peticiones
+        pass
+
+    def validacion_peticion(self, peticion):
+        if (peticion >= 0 and peticion <= 199):
+            return True
+        return False
+        pass
+
+    def ingreso_peticion_inicial(self):
+        while (True):
+            try:
+                entry_pet = int(input('Ingrese la peticion inicial : '))
+            except ValueError:
+                print('\nEl valor que ingreso no es un numero, por favor, vuelva a intentarlo.\n')
+                continue
+
+            if (self.validacion_peticion(entry_pet)):
+                self.inicial = entry_pet
+                break
+            else:
+                print('\nEl valor de la peticion ingresada no es valida, por favor, vuelva a ingresarlo.\n')
+                continue
+            pass  # pass de while
+        pass
+
+    def ingreso_peticiones(self):
+
+        num_peticiones = self.ingreso_num_peticiones()
+
+        print('\n\nLas peticiones deben tener valores entre 0 a 199\n\n')
+        self.ingreso_peticion_inicial()
+
+        while (num_peticiones != 0):
+            try:
+                entry_pet = int(input("Ingrese peticion : "))
+            except ValueError:
+                print('\nEl valor que ingreso no es un numero, por favor, vuelva a intentarlo.\n')
+                continue
+            if self.validacion_peticion(entry_pet):
+                self.buffer_datos.append(entry_pet)
+            else:
+                print('\nEl valor de la peticion ingresada no es valida, por favor, vuelva a ingresarlo.\n')
+                continue
+            num_peticiones -= 1
+            pass  # pass de while
+        pass  # pass de la funcion
+
+    def metodos(self, opc):
+        if opc == 1:
+            print("Poner la llamada de la función FCFS")
+        elif opc == 2:
+            tableau = SSTF.SSTF(self.buffer_datos, self.inicial)
+            self.print_tableau(tableau)
+        elif opc == 3:
+            print("Poner la llamada de la función SCAN")
+        elif opc == 4:
+            print("Poner la llamada de la función C-SCAN")
+        elif opc == 5:
+            print("Poner la llamada de la función LOOK")
+        elif opc == 6:
+            print("Poner la llamada de la función C-LOOK")
+        elif opc == 7:
+            self.isDone = True
+        pass
+
+    def menu(self):
+        menu = """\n
+============    MENÚ    ============
+
+
+SELECCIONE UNA OPCION:
+    [1] FCFS
+    [2] SSTF
+    [3] SCAN
+    [4] C-SCAN
+    [5] LOOK
+    [6] C-LOOK
+    [7] Salir
+    
+Del [1-7] que desea realizar? : """
+
+        while not self.isDone:
+            try:
+                opc = int(input(menu))
+            except ValueError:
+                print('\nEl valor que ingreso no es un numero, por favor, vuelva a intentarlo.\n')
+                continue
+            if (opc not in range(1, 8)):
+                print('\nLa opcion que ingreso no existe, por favor, vuelva a intentarlo.\n')
+                continue
+            self.metodos(opc)
+            pass
+
+        pass
+
+    def init(self):
+        #self.ingreso_peticiones()
+        self.menu()
+        pass
+
+    pass
+
+
+main = Main()
+
+main.init()
