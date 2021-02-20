35715bd9bce387419618c7f520d32424a4d1e6c2
Doncamatic1117 <dr.goku45@gmail.com>
2021-02-19 22:19:51 -0600

    Look y C Look agregados Grenas

---
+++ Grenas.py
@@ -0,0 +1,144 @@
+#Puro pendejo revisa este codigo
+#EL RETURN ESTA EN COMENTARIO PARA QUE NO CHINGUE XDDDD
+
+def LOOK(secuencia,inicial,tam):
+#INICIA CHAMBA*-----------------------------------------------------
+    print('LOOK**************************************')
+    izq = []
+    der = []
+    cActual = []
+    tEspera = []
+    desp = []
+    aux = 0
+
+    for i in range(tam):
+        if secuencia[i]<inicial:
+            izq.append(secuencia[i])
+        else:
+            der.append(secuencia[i])
+        pass
+
+    izq.sort()
+    izq.reverse()
+    der.sort()
+
+    cActual.append(inicial)
+    tEspera.append(0)
+
+    for i in range(len(izq)):
+        cActual.append(izq[i])
+        desp.append(cActual[i]-izq[i])
+        tEspera.append(tEspera[i]+desp[i])
+        pass
+
+    for i in range(len(der)):
+        cActual.append(der[i])
+        desp.append(der[i]-cActual[i+len(izq)])
+        tEspera.append(tEspera[i+len(izq)]+desp[i+len(izq)])
+        pass
+
+    cActual.pop()
+    tEspera.pop()
+
+    print('CILINDRO ACTUAL: ',cActual)
+    print('CILINDRO SOLICITADO: ',secuencia)
+    print('TIEMPO DE ESPERA: ',tEspera)
+    print('DESPLAZAMIENTO: ',desp)
+
+#return [cActual, secuencia, tEspera, desp]
+
+def CLOOK(secuencia,inicial,tam):
+#INICIA CHAMBA*-----------------------------------------------------
+    print('C-LOOK**************************************')
+    izq = []
+    der = []
+    cActual = []
+    tEspera = []
+    desp = []
+    aux = 0
+
+    for i in range(tam):
+        if secuencia[i]<inicial:
+            izq.append(secuencia[i])
+        else:
+            der.append(secuencia[i])
+        pass
+
+    izq.sort()
+    izq.reverse()
+    der.sort()
+    der.reverse()
+    aux = []
+    aux = izq + der
+
+    cActual.append(inicial)
+    tEspera.append(0)
+
+    for i in range(len(izq)):
+        cActual.append(izq[i])
+        desp.append(cActual[i]-izq[i])
+        tEspera.append(tEspera[i]+desp[i])
+        pass
+
+    for i in range(len(der)):
+        cActual.append(der[i])
+        desp.append(abs(cActual[i+len(izq)]-der[i]))
+        tEspera.append(tEspera[i+len(izq)]+desp[i+len(izq)])
+        pass
+
+    cActual.pop()
+    tEspera.pop()
+
+    print('CILINDRO ACTUAL: ',cActual)
+    print('CILINDRO SOLICITADO: ',aux)
+    print('TIEMPO DE ESPERA: ',tEspera)
+    print('DESPLAZAMIENTO: ',desp)
+
+#return [cActual, secuencia, tEspera, desp]
+
+def datos():
+    global secuencia
+    secuencia = []
+    global inicial
+    inicial = 0
+    flag = True
+    while flag==True:
+        print("Ingrese el tamano de su secuencia: ")
+        global tam
+        tam = 0
+        try:
+            tam=int(input('- '))
+            flag=False
+        except ValueError:
+            print('Solo se aceptan numeros enteros')
+    pass
+    flag=True
+
+    print('Ingrese su secuencia: ')
+    cont = 0
+
+    while cont<tam:
+        num = 0
+        try:
+            num=int(input('- '))
+            secuencia.append(num)
+            cont = cont+1
+        except ValueError:
+            print('Solo se aceptan numeros enteros')
+    pass
+
+    while flag==True:
+        print('Ingrese el valor inicial: ')
+        try:
+            inicial=int(input('- '))
+            flag=False
+        except ValueError:
+            print('Solo se aceptan numeros enteros')
+    pass
+
+datos()
+LOOK(secuencia,inicial,tam)
+CLOOK(secuencia,inicial,tam)
+
+#DOSCIENTAS POSICIONES MAX EN TAM
+#INICIAL LA MISMA RESTRICCION
